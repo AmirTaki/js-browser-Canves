381519f78ce922080c570be1672ac2c65ec727d4
amir <amirtaki623@gmail.com>
2025-05-11 23:50:28 +0330

    canvas

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let canvas = document.querySelector("canvas")
+
+canvas.width  = window.innerWidth / 2;
+canvas.height = window.innerWidth / 2;
+
+//method getContext
+let c = canvas.getContext("2d")
+
