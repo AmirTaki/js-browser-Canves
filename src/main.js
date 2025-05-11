c6a480f9aad7734e126fc67a989093fd200e4b9d
amir <amirtaki623@gmail.com>
2025-05-12 00:00:00 +0330

    Draw a rectangle

--- src/main.js
+++ src/main.js
@@ -6,3 +6,21 @@ canvas.height = window.innerWidth / 2;
 //method getContext
 let c = canvas.getContext("2d")
 
+
+c.fillText("Draw a rectangle", 100, 30)
+c.font = "30px verdana"
+c.translate(100, 100)  // Origin change point
+
+
+// Draw a rectangle
+// fillReact(distance left, distance top, Length, Width)
+
+c.fillStyle = "#27ae60"
+c.fillRect(100, 0, 100, 100 )
+
+c.fillStyle = "#3498db"
+c.fillRect(300, 100, 100, 100 )
+
+c.fillStyle = "#8e44ad"
+c.fillRect(500, 0, 100, 100 )
+
