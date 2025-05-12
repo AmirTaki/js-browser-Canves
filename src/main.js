1e593549b41bb3cbcd18bd8c9a384b147940b722
amir <amirtaki623@gmail.com>
2025-05-12 13:51:52 +0330

    draw a bow or circle : canvas

--- src/main.js
+++ src/main.js
@@ -6,25 +6,50 @@ canvas.height = window.innerWidth ;
 //method getContext
 let c = canvas.getContext("2d")
 
-// line drawing
+//Draw a bow or Draw a circle
+// arc (centerX, centerY, Radius,  bowStart, bowEnd)
 
-c.beginPath()                       // draw new
-c.lineTo(50, 300)                  //  point A
-c.lineTo(300, 100)                // point  B
-c.lineTo(400, 300)               //  point C
+/*
+P    : clock
+---------
+0    :   3
+.5p  :   6
+1p   :   9 
+1.5p :   12
+2p   :   3
 
-// method stroke :
-c.stroke.stroke = "#8e44ad"     // color
-c.closePath()                   // Connecting the first and last dots  
-c.stroke()                      // method draw
+*/
 
+c.beginPath()
+c.arc(300, 300, 50, Math.PI/2, 1.5 * Math.PI)
+c.stroke()
 
-
-c.beginPath()               // draw new
-c.lineTo(50, 400)          // point a
-c.lineTo(90, 600)          // point b
-c.lineTo(120, 299)          // point c
-
-// method fill style :
-c.fillStyle = "silver"
+c.beginPath()
+c.arc(400, 300, 70,  Math.PI/2, 2 *  Math.PI )
+c.fillStyle = "green"
 c.fill()
+
+
+
+c.beginPath()
+c.arc(300, 600, 60, Math.PI, Math.PI * 2 )
+c.fillStyle = "silver"
+c.fill();
+c.strokeStyle = "red"
+c.closePath()
+c.stroke()
+
+// c.clearRect(100, 200, 300, 400)  // clear canves
+c.clearRect(0, 0, window.innerWidth, window.innerHeight)  // clear canves
+
+
+for (let i = 0 ; i < 70 ; i++){
+    X = Math.random() * window.innerWidth    
+    Y = Math.random() * window.innerHeight 
+
+    c.beginPath()
+    c.arc(X, Y, 30, 0, Math.PI * 2)
+    c.strokeStyle = "aqua";
+    c.stroke()
+}
+
