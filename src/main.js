f7e12f4e5a16ee554fa433683c07c24262952b93
amir <amirtaki623@gmail.com>
2025-05-12 15:15:54 +0330

    animation part 3 ( OPP ) :Canvas

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 
 let canvas = document.querySelector("canvas")
 
-canvas.width  = window.innerWidth   ;
-canvas.height = window.innerWidth  ;
+canvas.width  = window.innerWidth    ;
+canvas.height = window.innerHeight   ;
 
 //method getContext
 let c = canvas.getContext("2d")
@@ -12,33 +12,45 @@ let c = canvas.getContext("2d")
 // randomIntevall numbers
 const randomIntFromInterval = (min, max) =>  { return Math.floor(Math.random() * (max - min + 1) + min)}
 
-// Making animation
-let r = 15;
-let x = randomIntFromInterval(0 + r, window.innerWidth - r);
-let y = randomIntFromInterval(0 + r, window.innerHeight - r);
-let vx = (Math.random() - .5) * 6
-let vy = (Math.random() - .5) * 6
+// Making animation : object Oriented Programming
+class Ball {
+    constructor(){
+        this.r = 10
+        this.x = randomIntFromInterval(0 + this.r, window.innerWidth - this.r);
+        this.y = randomIntFromInterval(0 + this.r, window.innerHeight - this.r);
+        this.vx = (Math.random() - .5) * 4
+        this.vy = (Math.random() - .5) * 4
+        this.color = ['black', 'blue', "brown", 'green', 'red', 'yellow', 'pink', 'silver']
+        this.contColor = randomIntFromInterval(0, this.color.length -1)
+        this.draw()
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, true)
+        c.fillStyle = this.color[this.contColor]
+        c.fill()
+    }
+    update(){
+        this.vx = this.x +  this.r > window.innerWidth  || this.x - this.r < 0 ? - this.vx : this.vx
+        this.vy = this.y +  this.r > window.innerHeight || this.y - this.r < 0 ? - this.vy : this.vy
 
-function animate(){
-    c.clearRect(0, 0, window.innerWidth, window.innerHeight)
-    c.beginPath()
-    c.arc(x, y, r, 0, 2 * Math.PI, true)
-    c.fillStyle = "blue"
-    c.fill()
-
-    // if (x + r > window.innerWidth || x - r < 0){
-    //     vx = -vx
-    // }
-
-    vx = x + r > window.innerWidth || x - r < 0 ? -vx : vx
-    vy = y + r > window.innerHeight || y - r < 0 ? -vy : vy
-
-    x += vx
-    y += vy
-    requestAnimationFrame(animate)
+        this.x += this.vx
+        this.y += this.vy
+        this.draw()
+    }
 }
 
+let balls = []
+for (let i = 0 ; i < 50; i++){
+    balls.push(new Ball)
+}
 
-animate()
-
-
+// animate
+const animate = () => {
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    balls.forEach(ball =>{
+        ball.update()
+    })
+    requestAnimationFrame(animate)
+}
+animate();
